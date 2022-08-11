ea966991c22164de8baa97e766c0cc4e5ecedf28
Tatsuya Sakamoto <tatsuya.sakamoto@jmty.jp>
2022-08-11 15:31:59 +0900

    URLSchemeでcalled画面に遷移

--- url_scheme_caller/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ url_scheme_caller/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.myapplication
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setupBinding()
+        setupUrlSchemeButton()
+    }
+
+    private fun setupBinding() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+    private fun setupUrlSchemeButton() {
+        binding.buttonUrlScheme.setOnClickListener {
+            val uri = Uri.parse("tatsuya://host")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
     }
 }
